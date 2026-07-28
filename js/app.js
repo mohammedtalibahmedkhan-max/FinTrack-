738126eded233bf7f45314396854c86b480b6324
Mohd Talib Ahmed Khan <mohammedtalibahmedkhan@gmail.com>
2026-07-28 11:48:32 +0530

    filter and sort history features added

--- js/app.js
+++ js/app.js
@@ -1,50 +1,55 @@
-/*==========================================================
+/*
+==========================================================
 PROJECT : FinTrack V3
 
 FILE : app.js
 
 PURPOSE
 
-Main JavaScript file.
+Main JavaScript File
 
-Controls
+Handles
 
-- Form
-- Dashboard
-- Transactions
+✓ DOM Selection
+✓ CRUD
+✓ Dashboard
+✓ Local Storage
+✓ Search
+✓ Filters
+✓ Sorting
+==========================================================
+*/
 
+
+/*==========================================================
+DOM ELEMENTS
 ==========================================================*/
 
-//SELECT HTML ELEMENTS
-   
+// Transaction Form
+const transactionForm =
+document.getElementById("transaction-form");
 
-//transactin form 
-const transactionForm = document.getElementById("transaction-form");
+// Form Inputs
+const titleInput =
+document.getElementById("title");
 
-//title input
-const titleInput = document.getElementById("title");
+const amountInput =
+document.getElementById("amount");
 
-//amount input 
-const amountInput = document.getElementById("amount");
+const categoryInput =
+document.getElementById("category");
 
-//category
-const categoryInput = document.getElementById("category");
+const typeInput =
+document.getElementById("type");
 
-//type
-const typeInput = document.getElementById("type");
+const dateInput =
+document.getElementById("date");
 
-//date
-const dateInput = document.getElementById("date");
+// Transaction Table
+const transactionList =
+document.getElementById("transaction-list");
 
-
-// Transaction Table Body
-const transactionList = document.getElementById("transaction-list");
-
-
-//==========================================================
-// DASHBOARD ELEMENTS
-//==========================================================
-
+// Dashboard
 const balanceElement =
 document.getElementById("balance");
 
@@ -57,163 +62,293 @@ document.getElementById("expense");
 const savingsElement =
 document.getElementById("savings");
 
-//==========================================================
-// APPLICATION DATA
-//==========================================================
+// Submit Button
+const submitButton =
+document.querySelector(".submit-btn");
 
-// Array to store every transaction
+// Search & Filters
+const searchInput =
+document.getElementById("search-input");
+
+const categoryFilter =
+document.getElementById("category-filter");
+
+const typeFilter =
+document.getElementById("type-filter");
+
+const sortFilter =
+document.getElementById("sort-filter");
+
+
+/*==========================================================
+APPLICATION STATE
+==========================================================*/
+
+// Stores every transaction
 let transactions = [];
 
-//==========================================================
-// EVENT LISTENER
-//==========================================================
+// Stores the ID of the transaction currently being edited
+let editingTransactionId = null;
 
+
+/*==========================================================
+EVENT LISTENERS
+==========================================================*/
+
+// Submit Form
 transactionForm.addEventListener(
-
     "submit",
-
     addTransaction
-
 );
-//==========================================================
-// ADD TRANSACTION
-//==========================================================
+
+
+/*==========================================================
+ADD / UPDATE TRANSACTION
+==========================================================*/
 
 function addTransaction(event){
 
-    // Stop page refresh
+    // Prevent page refresh
     event.preventDefault();
 
-    // Read values from the form
-const title = titleInput.value.trim();
 
-const amount = Number(amountInput.value);
+    /*--------------------------------------
+    READ FORM VALUES
+    --------------------------------------*/
 
-const category = categoryInput.value;
+    const title =
+    titleInput.value.trim();
 
-const type = typeInput.value;
+    const amount =
+    Number(amountInput.value);
 
-const date = dateInput.value;
+    const category =
+    categoryInput.value;
+
+    const type =
+    typeInput.value;
+
+    const date =
+    dateInput.value;
 
 
-//==========================================================
-// CREATE TRANSACTION OBJECT
-//==========================================================
+    /*--------------------------------------
+    VALIDATION
+    --------------------------------------*/
 
-const transaction = {
+    if(title === ""){
 
-    id: Date.now(),
+        alert("Please enter a title.");
 
-    title: title,
+        return;
 
-    amount: amount,
+    }
 
-    category: category,
+    if(isNaN(amount) || amount <= 0){
 
-    type: type,
+        alert("Please enter a valid amount.");
 
-    date: date
+        return;
 
-};
+    }
 
-//==========================================================
-// STORE TRANSACTION
-//==========================================================
+    if(date === ""){
 
-transactions.push(transaction);
-saveTransactions();
-console.log(transactions);
+        alert("Please select a date.");
 
-displayTransactions();
+        return;
 
-updateDashboard();
-//==========================================================
-// RESET FORM
-//==========================================================
+    }
 
-transactionForm.reset();
 
-// Display values in the Console
-console.log(title);
-console.log(amount);
-console.log(category);
-console.log(type);
-console.log(date);
+    /*--------------------------------------
+    EDIT MODE
+    --------------------------------------*/
+
+    if(editingTransactionId !== null){
+
+        const transaction =
+        transactions.find(function(item){
+
+            return item.id === editingTransactionId;
+
+        });
+
+        if(transaction){
+
+            transaction.title = title;
+
+            transaction.amount = amount;
+
+            transaction.category = category;
+
+            transaction.type = type;
+
+            transaction.date = date;
+
+        }
+
+        editingTransactionId = null;
+
+        submitButton.textContent =
+        "Add Transaction";
+
+        saveTransactions();
+
+        filterTransactions();
+
+        updateDashboard();
+
+        transactionForm.reset();
+
+        return;
+
+    }
+
+
+    /*--------------------------------------
+    CREATE NEW TRANSACTION
+    --------------------------------------*/
+
+    const transaction = {
+
+        id: Date.now(),
+
+        title: title,
+
+        amount: amount,
+
+        category: category,
+
+        type: type,
+
+        date: date
+
+    };
+
+
+    /*--------------------------------------
+    SAVE TRANSACTION
+    --------------------------------------*/
+
+    transactions.push(transaction);
+
+    saveTransactions();
+
+    filterTransactions();
+
+    updateDashboard();
+
+    transactionForm.reset();
 
 }
 
+/*==========================================================
+DISPLAY TRANSACTIONS
+==========================================================*/
 
-//==========================================================
-// DISPLAY TRANSACTIONS
-//==========================================================
+function displayTransactions(list = transactions){
 
-function displayTransactions() {
-
-    // Clear the current table
+    // Remove old rows
     transactionList.innerHTML = "";
 
+    // Variable to store all rows
+    let rows = "";
+
     // Loop through every transaction
-    transactions.forEach(function(transaction) {
+    list.forEach(function(transaction){
 
-        const row = `
-            <tr>
-                <td>${transaction.title}</td>
-                <td>${transaction.category}</td>
-                <td>${transaction.type}</td>
-                <td>₹${transaction.amount.toFixed(2)}</td>
-                <td>${transaction.date}
-                <td>
-                <button class="delete-btn" onclick="deleteTransaction(${transaction.id})">Delete</button>
-                </td>
-            </tr>
+        rows += `
+
+        <tr>
+
+            <td>${transaction.title}</td>
+
+            <td>${transaction.category}</td>
+
+            <td>${transaction.type}</td>
+
+            <td>₹${transaction.amount.toFixed(2)}</td>
+
+            <td>${transaction.date}</td>
+
+            <td>
+
+                <button
+                    class="edit-btn"
+                    onclick="editTransaction(${transaction.id})">
+
+                    Edit
+
+                </button>
+
+                <button
+                    class="delete-btn"
+                    onclick="deleteTransaction(${transaction.id})">
+
+                    Delete
+
+                </button>
+
+            </td>
+
+        </tr>
+
         `;
-
-        transactionList.innerHTML += row;
 
     });
 
+    transactionList.innerHTML = rows;
+
 }
-
-
-//update dashboard
-  
+/*==========================================================
+UPDATE DASHBOARD
+==========================================================*/
 
 function updateDashboard(){
-    let income =0;
+
+    let income = 0;
+
     let expense = 0;
+
     transactions.forEach(function(transaction){
 
-    if(transaction.type === "Income"){
+        if(transaction.type === "Income"){
 
-        income += transaction.amount;
+            income += transaction.amount;
 
-    }
+        }
 
-    else{
+        else{
 
-        expense += transaction.amount;
+            expense += transaction.amount;
 
-    }
+        }
 
-});
-const balance = income - expense;
-const savings = balance;
+    });
 
-balanceElement.textContent =
-`₹${balance.toFixed(2)}`;
+    const balance = income - expense;
 
-incomeElement.textContent =
-`₹${income.toFixed(2)}`;
+    const savings = balance;
 
-expenseElement.textContent =
-`₹${expense.toFixed(2)}`;
+    balanceElement.textContent =
+    `₹${balance.toFixed(2)}`;
 
-savingsElement.textContent =
-`₹${savings.toFixed(2)}`;
+    incomeElement.textContent =
+    `₹${income.toFixed(2)}`;
+
+    expenseElement.textContent =
+    `₹${expense.toFixed(2)}`;
+
+    savingsElement.textContent =
+    `₹${savings.toFixed(2)}`;
+
 }
 
 
-//save to local storage
+/*==========================================================
+SAVE TRANSACTIONS
+==========================================================*/
  function saveTransactions(){
     localStorage.setItem("transactions", JSON.stringify(transactions));
  }
@@ -224,15 +359,15 @@ savingsElement.textContent =
     const savedTransactions = localStorage.getItem("transactions");
     if(savedTransactions){
         transactions= JSON.parse(savedTransactions);
-        displayTransactions();
-        updateDashboard();
     }
+       filterTransactions();
+        updateDashboard();
  }
 
 
- //==========================================================
+
 // DELETE TRANSACTION
-//==========================================================
+
 
 function deleteTransaction(id){
      const confirmed = confirm(
@@ -253,13 +388,192 @@ function deleteTransaction(id){
 
     saveTransactions();
 
-    displayTransactions();
+    filterTransactions();
 
     updateDashboard();
 
 }
- //==========================================================
+
+//edit transactions
+function editTransaction(id){
+    const transaction = transactions.find(function(item){
+        return item.id === id;
+    });
+    if(!transaction){
+        return;
+    }
+    titleInput.value = transaction.title;
+    amountInput.value = transaction.amount;
+    categoryInput.value = transaction.category;
+    typeInput.value = transaction.type;
+    dateInput.value = transaction.date;
+    editingTransactionId = id;
+    submitButton.textContent = "Update Transaction";
+} 
+
+/*==========================================================
+FILTER + SEARCH + SORT
+==========================================================*/
+
+function filterTransactions(){
+
+    /*--------------------------------------
+    READ FILTER VALUES
+    --------------------------------------*/
+
+    const searchText =
+    searchInput.value.toLowerCase();
+
+    const selectedCategory =
+    categoryFilter.value;
+
+    const selectedType =
+    typeFilter.value;
+
+    const selectedSort =
+    sortFilter.value;
+
+
+    /*--------------------------------------
+    FILTER TRANSACTIONS
+    --------------------------------------*/
+
+    const filteredTransactions =
+    transactions.filter(function(transaction){
+
+        const matchesSearch =
+
+            transaction.title
+            .toLowerCase()
+            .includes(searchText);
+
+        const matchesCategory =
+
+            selectedCategory === "All"
+
+            ||
+
+            transaction.category === selectedCategory;
+
+        const matchesType =
+
+            selectedType === "All"
+
+            ||
+
+            transaction.type === selectedType;
+
+        return (
+
+            matchesSearch
+
+            &&
+
+            matchesCategory
+
+            &&
+
+            matchesType
+
+        );
+
+    });
+
+
+    /*--------------------------------------
+    CREATE COPY FOR SORTING
+    --------------------------------------*/
+
+    let sortedTransactions =
+    [...filteredTransactions];
+
+
+    /*--------------------------------------
+    SORT BY AMOUNT
+    --------------------------------------*/
+
+    if(selectedSort === "amount-asc"){
+
+        sortedTransactions.sort(function(a,b){
+
+            return a.amount - b.amount;
+
+        });
+
+    }
+
+    if(selectedSort === "amount-desc"){
+
+        sortedTransactions.sort(function(a,b){
+
+            return b.amount - a.amount;
+
+        });
+
+    }
+
+
+    /*--------------------------------------
+    SORT BY DATE
+    --------------------------------------*/
+
+    if(selectedSort === "date-new"){
+
+        sortedTransactions.sort(function(a,b){
+
+            return new Date(b.date) -
+
+                   new Date(a.date);
+
+        });
+
+    }
+
+    if(selectedSort === "date-old"){
+
+        sortedTransactions.sort(function(a,b){
+
+            return new Date(a.date) -
+
+                   new Date(b.date);
+
+        });
+
+    }
+
+
+    /*--------------------------------------
+    DISPLAY RESULT
+    --------------------------------------*/
+
+    displayTransactions(sortedTransactions);
+
+}
+
+searchInput.addEventListener(
+    "input",
+    filterTransactions
+);
+
+categoryFilter.addEventListener(
+    "change",
+    filterTransactions
+);
+
+typeFilter.addEventListener(
+    "change",
+    filterTransactions
+);
+
+sortFilter.addEventListener(
+
+    "change",
+
+    filterTransactions
+
+);
+
 // INITIALIZE APPLICATION
-//==========================================================
+
 
 loadTransactions();
